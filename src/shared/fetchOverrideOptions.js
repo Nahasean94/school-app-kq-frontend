58f10b72f8cc89abc2dd901f9e95812a00939792
Nahashon Njenga <ncubed940@gmail.com>
2019-01-14 23:44:15 +0300

    Changed graphql host

--- src/shared/fetchOverrideOptions.js
+++ src/shared/fetchOverrideOptions.js
@@ -1,5 +1,5 @@
 export function fetchOptionsOverride(options) {
-    options.url = 'http://localhost:8080/graphql'
+    options.url = 'http://68.183.29.191:8080/graphql'
     const token = localStorage.getItem('SchoolSystem')
     if (token) options.headers.Authorization = `Bearer ${token}`
 }
